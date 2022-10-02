e72de012abc2f35a0142a870a7bdc81b92177dce
Elvijs <elvijspodnieks.com>
2022-10-02 21:37:30 +0300

    Date papildinajums

--- homeex/src/io/codelex/practice/DatePeriod.java
+++ homeex/src/io/codelex/practice/DatePeriod.java
@@ -11,5 +11,35 @@ public class DatePeriod {
         this.periodEnd = end;
     }
 
+    public DatePeriod intersection(DatePeriod period) {
+        LocalDate pStart = this.periodStart;
+        LocalDate pEnd = this.periodEnd;
+        if (pStart.isBefore(period.periodStart) && pEnd.isAfter(period.periodEnd)) {
+            return period;
+        }
+        if (pStart.isAfter(period.periodStart) && pEnd.isBefore(period.periodEnd)) {
+            return this;
+        }
+        if (pStart.isAfter(period.periodEnd) || pEnd.isBefore(period.periodStart)) {
+            return null;
+        }
+        if (pStart.isBefore(period.periodStart)) {
+            pStart = period.periodStart;
+        }
+        if (pEnd.isAfter(period.periodEnd)) {
+            pEnd = period.periodEnd;
+        }
+        return new DatePeriod(pStart, pEnd);
+
+
+    }
+
+    public String toString() {
+        return "DatePeriod{" +
+                "periodStart=" + periodStart +
+                ", periodEnd=" + periodEnd +
+                '}';
+    }
+
 
 }
